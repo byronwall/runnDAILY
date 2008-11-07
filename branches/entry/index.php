662ae0a5ba4646e193d6a79cd00e7e5ea06d36a9
chandler <chandler@bce454a7-8c10-b24f-9796-9f7183d4e6d1>
2008-11-07 00:23:04 +0000

    More updates.

--- branches/entry/index.php
+++ branches/entry/index.php
@@ -1,12 +1,9 @@
 <?php
 require("lib/config.php");
 
-$smarty->assign("debugMode", 0);
-$smarty->assign("debugData", $DEBUG);
-
 $content = $smarty->fetch("index.tpl");
 $smarty->assign("page_content", $content);
 
 $smarty->assign("page_title", "Running Site");
-$smarty->display('master.tpl');
+$smarty->display("master.tpl");
 ?>
