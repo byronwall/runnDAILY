a8224bba314166bba38700cdf5633d54b5c23eff
byron <byron@bce454a7-8c10-b24f-9796-9f7183d4e6d1>
2008-11-10 12:42:18 +0000

    Renamed the parts of code that were referencing the old SQL names.

--- branches/entry/lib/class/class_route.php
+++ branches/entry/lib/class/class_route.php
@@ -26,7 +26,7 @@ class Route{
 	 */
 	function createNewRoute(User $user, $distance, $points, $comments, $name){
 		$stmt = $this->mysqli->stmt_init();
-		$stmt->prepare("INSERT INTO running.routes (uid ,distance ,points ,comments ,name ,creation) VALUES(?, ?,?,?,?, NOW())") or die($stmt->error);
+		$stmt->prepare("INSERT INTO running.routes (r_uid ,r_distance ,r_points ,r_comments ,r_name ,r_creation) VALUES(?, ?,?,?,?, NOW())") or die($stmt->error);
 		$stmt->bind_param("idsss", $user->userID, $distance, $points, $comments, $name) or die($stmt->error);
 
 		$stmt->execute() or die($stmt->error);
@@ -52,15 +52,15 @@ class Route{
 	public static function getRoutesForUser(User $user){
 		$mysqli = database::getDB();
 
-		$stmt = $mysqli->prepare("SELECT routes.name, id FROM routes WHERE uid=?") or die("error:".$stmt->error);
+		$stmt = $mysqli->prepare("SELECT r_name, r_id FROM routes WHERE r_uid=?") or die("error:".$stmt->error);
 		$stmt->bind_param("i", $user->userID) or die("error:".$stmt->error);
 
 		$stmt->execute() or die("error:".$stmt->error);
 		$stmt->store_result();
 
 		while ($row = $stmt->fetch_assoc()) {
-			$route = new Route($row["name"]);
-			$route->id = $row["id"];
+			$route = new Route($row["r_name"]);
+			$route->id = $row["r_id"];
 			$output[] = $route;
 		}
 
@@ -77,7 +77,7 @@ class Route{
 	public static function getAllRoutes(){
 		$mysqli = database::getDB();
 
-		$stmt = $mysqli->prepare("SELECT name, id, distance FROM routes");
+		$stmt = $mysqli->prepare("SELECT r_name, r_id, r_distance FROM routes");
 
 		$stmt->execute();
 		$stmt->store_result();
@@ -85,9 +85,9 @@ class Route{
 		$routes = array();
 
 		while($row = $stmt->fetch_assoc()){
-			$route = new Route($row["name"]);
-			$route->id = $row["id"];
-			$route->distance = $row["distance"];
+			$route = new Route($row["r_name"]);
+			$route->id = $row["r_id"];
+			$route->distance = $row["r_distance"];
 
 			$routes[] = $route;
 		}
@@ -105,7 +105,7 @@ class Route{
 	public static function fromRouteIdentifier($id){
 		$mysqli = database::getDB();
 
-		$stmt = $mysqli->prepare("SELECT points, name, distance FROM routes WHERE id=?");
+		$stmt = $mysqli->prepare("SELECT r_points, r_name, r_distance FROM routes WHERE r_id=?");
 		$stmt->bind_param("i", $id);
 
 		$stmt->execute();
@@ -113,8 +113,8 @@ class Route{
 
 		$row = $stmt->fetch_assoc();
 
-		$route = new Route($row["name"]);
-		$route->points = $row["points"];
+		$route = new Route($row["r_name"]);
+		$route->points = $row["r_points"];
 
 
 		$stmt->close();
