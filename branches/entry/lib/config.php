a75c6f824fc8c1b21b4d3f2b1c653b55d9fddf84
chandler <chandler@bce454a7-8c10-b24f-9796-9f7183d4e6d1>
2008-11-19 06:12:35 +0000

    Temporary "hack" to bypass google's map key.

--- branches/entry/lib/config.php
+++ branches/entry/lib/config.php
@@ -1,9 +1,11 @@
 <?php
 $site_root = dirname(dirname(__FILE__));
+$google_id = ABQIAAAAYZcibhuwr8GMgCWYwqU-RxQzNv4mzrEKtvvUg4SKGFnPU6pUNBTkQL_qSiLmJQ3qE-zNxRFJgRZM8g
 
 require_once($site_root."/lib/class/ext_mysqli.php");
 require_once($site_root."/lib/class/class_user.php");
 require_once($site_root."/lib/class/class_route.php");
+require_once($site_root."/lib/class/config_special.php")
 
 require_once($site_root."/_smarty/Smarty.class.php");
 
