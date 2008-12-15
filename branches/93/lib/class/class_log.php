55644533c02313d40b0ca61e7659e62912ddf517
chandler <chandler@bce454a7-8c10-b24f-9796-9f7183d4e6d1>
2008-12-15 18:08:23 +0000

    Unecessary reference function has been removed.

--- branches/93/lib/class/class_log.php
+++ branches/93/lib/class/class_log.php
@@ -15,21 +15,5 @@ class Log{
 		return false;
 		
 	}
-	
-	function createNewRoute(User $user, $distance, $points, $comments, $name, $start_lat, $start_lng){
-		$stmt = database::getDB()->stmt_init();
-		$stmt->prepare("INSERT INTO running.routes (r_uid ,r_distance ,r_points ,r_description ,r_name ,r_creation, r_start_lat, r_start_lng) VALUES(?, ?,?,?,?, NOW(),?,?)") or die($stmt->error);
-		$stmt->bind_param("idsssdd", $user->userID, $distance, $points, $comments, $name, $start_lat, $start_lng) or die($stmt->error);
-
-		$stmt->execute() or die($stmt->error);
-		
-		if($stmt->affected_rows == 1){
-			$ins_id = $stmt->insert_id;
-			$stmt->close();
-			$user->logActivity("route", "created route ". $ins_id);
-			return $ins_id;
-		}
-		return false;
-	}	
 }
 ?>
