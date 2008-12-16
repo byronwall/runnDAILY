3b3ac4693522bd28883eb8bbd20d1e2d090ac987
byron <byron@bce454a7-8c10-b24f-9796-9f7183d4e6d1>
2008-12-16 05:44:34 +0000

    Includes the changes to add RSS feed support to our website.  I will document and detail soon, but here are the basics.

--- branches/95/lib/config_class.php
+++ branches/95/lib/config_class.php
@@ -4,4 +4,5 @@ $site_root = dirname(dirname(__FILE__));
 require_once($site_root."/lib/class/ext_mysqli.php");
 require_once($site_root."/lib/class/class_user.php");
 require_once($site_root."/lib/class/class_route.php");
+require_once($site_root."/lib/class/class_rss.php");
 ?>
---
+++ branches/95/rss/rss_generic.php
@@ -0,0 +1,16 @@
+<?php
+require("../lib/config.php");
+
+$feed = new RssFeed("runndaily.com", "http://byron.byroni.us", "running site");
+$feed->defineImageForFeed(new RssImage("running logo", "http://byroni.us", "http://runndaily.com/img/logo.png"));
+$feed->addItemToFeed(new RssItem("byron is cool", "001", "http://byroni.us", "byron is cool"));
+$feed->addItemToFeed(new RssItem("byron is cool", "002", "http://byroni.us", "byron is cool"));
+$feed->addItemToFeed(new RssItem("byron is cool", "003", "http://byroni.us", "byron is cool"));
+
+$smarty->assign("RssFeed", $feed);
+
+$rss_out = $smarty->fetch("rss/generic.tpl");
+
+header("Content-Type: application/rss+xml");
+echo $rss_out;
+?>
