dd435eaf33b2f7d19d7b91de8ecc1d3b4076378b
chandler <chandler@bce454a7-8c10-b24f-9796-9f7183d4e6d1>
2008-12-18 17:08:08 +0000

    Branch for #117 - Create a new favicon. See #117.

---
+++ branches/117 - Favicon/lib/config_class.php
@@ -0,0 +1,7 @@
+<?php
+$site_root = dirname(dirname(__FILE__));
+
+require_once($site_root."/lib/class/ext_mysqli.php");
+require_once($site_root."/lib/class/class_user.php");
+require_once($site_root."/lib/class/class_route.php");
+?>
