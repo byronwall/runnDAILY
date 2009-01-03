c377a69942b1eff61a41bec101d65c3bc70ccc34
byron <byron@bce454a7-8c10-b24f-9796-9f7183d4e6d1>
2009-01-03 23:50:18 +0000

    Reintegrates branch 123.  This moves the Smarty settings into the Smarty class to clean up our files and to allow for a quick Smarty object creation.  See #123.

--- trunk/lib/config.php
+++ trunk/lib/config.php
@@ -1,17 +1,10 @@
 <?php
 require("config_class.php");
 
-require_once($site_root."/_smarty/Smarty.class.php");
+require_once(SITE_ROOT."/_smarty/Smarty.class.php");
 
 /*SET UP TEMPLATING ENGINE*/
 $smarty = new Smarty();
-
-$smarty->template_dir = $site_root."/tpl";
-$smarty->compile_dir = $site_root."/_smarty/templates_c";
-$smarty->cache_dir = $site_root."/_smarty/cache";
-$smarty->config_dir = $site_root."/_smarty/configs";
-$smarty->left_delimiter = "{{";
-$smarty->right_delimiter = "}}";
 
 /*VALIDATE THE USER EVERYTIME*/
 session_start();
