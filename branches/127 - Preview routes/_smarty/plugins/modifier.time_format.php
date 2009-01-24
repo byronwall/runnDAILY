26ebffef4bfaf4ab27e0a9751678cb7802f5a493
byron <byron@bce454a7-8c10-b24f-9796-9f7183d4e6d1>
2009-01-24 03:27:10 +0000

    Removing completed branches.

--- branches/127 - Preview routes/_smarty/plugins/modifier.time_format.php
+++
@@ -1,5 +0,0 @@
-<?php
-function smarty_modifier_time_format($seconds){
-	return date("H:i:s", mktime(0,0,$seconds, 1, 1, 2009));
-}
-?>
--- branches/156 - Feedback form on every page/admin/stats.php
+++
@@ -1,6 +0,0 @@
-<?php
-require("../lib/config.php");
-
-$smarty->assign("stats",Stats::getRecentStats());
-$smarty->display_master("admin/stats.tpl");
-?>
