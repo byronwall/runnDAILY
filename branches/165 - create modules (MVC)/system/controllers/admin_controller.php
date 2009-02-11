b80235de54a0638007d3783e27aa1c409c008c78
byron <byron@bce454a7-8c10-b24f-9796-9f7183d4e6d1>
2009-02-11 22:25:50 +0000

    See #165.
    Branching to work on ticket 165.  This will add modules.

---
+++ branches/165 - create modules (MVC)/system/controllers/admin_controller.php
@@ -0,0 +1,69 @@
+<?php
+class admin_controller{
+	public function feedback(){
+		$result = Database::getDB()->query("
+			SELECT * FROM messages as m
+			LEFT JOIN users ON u_uid = m_uid_from
+			WHERE m.m_uid_to = 0
+		");
+		$msgs = array();
+		
+		while($row = $result->fetch_assoc()){
+			$msgs[] = new Message($row);
+		}
+		
+		$result->close();
+		
+		RoutingEngine::getSmarty()->assign("message", $msgs);
+	}
+	public function index(){
+		
+	}
+	public function pages(){
+		RoutingEngine::getSmarty()->assign("pages", Page::getAllPages());
+	}
+	public function stats(){
+		RoutingEngine::getSmarty()->assign("stats",Stats::getRecentStats());
+	}
+	public function users(){
+		$parser = new SqlParser(true, 10, 0);
+		$parser->addCondition(new SqlRangeCondition("u_date_access", "FROM_UNIXTIME", "strtotime"));
+		$parser->addCondition(new SqlLikeCondition("u_username"));
+		$parser->addCondition(new SqlLikeCondition("u_email"));
+		$parser->addCondition(new SqlEqualCondition("u_uid"));
+		$parser->addCondition(new SqlRangeCondition("u_type"));
+		$parser->setData($_GET);
+		
+		$stmt = Database::getDB()->prepare("
+			SELECT *
+			FROM users
+			WHERE 
+				{$parser->getSQL()}
+		");
+		
+		$parser->bindParamToStmt($stmt);
+		$stmt->execute();
+		$stmt->store_result();
+		
+		$users = array();
+		while($row = $stmt->fetch_assoc()){
+			$users[] = new User($row);
+		}
+		
+		RoutingEngine::getSmarty()->assign("users", $users);
+	}
+	public function update_stats(){
+		if(Stats::insertStats()){
+			exit("success");
+		}
+		echo "DID NOT WORK";		
+	}
+	public function update_page(){
+		$p_page = new Page($_POST, "p_");
+		if($p_page->updatePage()){
+			exit("success");
+		}
+		exit("DID NOT UPDATE");
+	}
+}
+?>
---
+++ branches/165 - create modules (MVC)/system/tpl/help/_pages/route_create.tpl
@@ -0,0 +1,6 @@
+<h2>creating a route</h2>
+
+<p>
+creating a route is done using the create a route page.
+simply click around on the map to draw the route.
+</p>
