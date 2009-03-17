ce7f78211bccde64e2e8bbf9e69ee0218e28e5d1
byron <byron@bce454a7-8c10-b24f-9796-9f7183d4e6d1>
2009-03-17 20:32:55 +0000

    Authentication of users has been moved into the RoutingEngine and out of the default index page.  Just a little more cleanup.

--- trunk/public/index.php
+++ trunk/public/index.php
@@ -9,17 +9,7 @@ DEFINE("CLASS_ROOT", SYSTEM_ROOT."/class");
 require(SYSTEM_ROOT."/config.php");
 session_start();
 
-if(isset($_SESSION["userData"]) && $_SESSION["userData"]->uid){
-	User::$current_user = $_SESSION["userData"];
-	User::$current_user->refreshDetails();
-}
-else{
-	$_SESSION["userData"] = User::cookieLogin();
-	User::$current_user = $_SESSION["userData"];
-}
-if(!isset(User::$current_user->permissions)){
-	User::$current_user->getPermissions();
-}
+RoutingEngine::getInstance()->authenticateUser();
 
 $request = explode("?", $_SERVER["REQUEST_URI"]);
 
