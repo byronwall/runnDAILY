daffe5872273af28d3b06ea14c479dd844afadb5
Chandler Wall <c@swall.us>
2009-03-25 12:45:05 +0000

    Connection settings for chandler's computer.

--- trunk/system/config.php
+++ trunk/system/config.php
@@ -1,10 +1,9 @@
 <?php
-
 DEFINE("COOKIE_NAME", "runndaily");
-DEFINE("DB_USER", "db64581");
-DEFINE("DB_PASS", "j6Hmj544vUETZedU");
-DEFINE("DB_NAME", "db64581_running");
-DEFINE("DB_HOST", $_ENV["DATABASE_SERVER"]);
+DEFINE("DB_USER", "thechanmane");
+DEFINE("DB_PASS", "ic'an'cu88");
+DEFINE("DB_NAME", "running");
+DEFINE("DB_HOST", "98.223.231.125");
 spl_autoload_register("site_autoload");
 
 function site_autoload($class){
