ee35a6ef9f0111d8d0026ea2d492a718b3b86687
byron wall <byron@byroni.us>
2009-03-25 20:57:56 +0000

    Another fix.

--- html/index.php
+++ html/index.php
@@ -7,6 +7,10 @@ DEFINE("PUBLIC_ROOT", dirname(__FILE__));
 DEFINE("ROOT", dirname(PUBLIC_ROOT));
 DEFINE("SYSTEM_ROOT", ROOT . "/system");
 DEFINE("CLASS_ROOT", SYSTEM_ROOT."/class");
+DEFINE("DB_USER", "db64581");
+DEFINE("DB_PASS", "byron3chan");
+DEFINE("DB_NAME", "db64581_running");
+DEFINE("DB_HOST", $_ENV["DATABASE_SERVER"]);
 
 require(SYSTEM_ROOT."/config.php");
 session_start();
@@ -15,20 +19,6 @@ if(array_safe($_GET, "mediatemplebackdoor", false)){
 	$_SESSION["DbBackdoor"] = true;
 }
 
-/*
-if(array_safe($_SESSION, "DbBackdoor", false)){
-	DEFINE("DB_USER", "db64581");
-	DEFINE("DB_PASS", "byron3chan");
-	DEFINE("DB_NAME", "db64581_running");
-	DEFINE("DB_HOST", $_ENV["DATABASE_SERVER"]);
-}*/
-//else{
-	DEFINE("DB_USER", "thechanmane");
-	DEFINE("DB_PASS", "ic'an'cu88");
-	DEFINE("DB_NAME", "running");
-	DEFINE("DB_HOST", "98.223.231.125");
-	
-//}
 RoutingEngine::getInstance()->authenticateUser();
 
 $request = explode("?", $_SERVER["REQUEST_URI"]);
