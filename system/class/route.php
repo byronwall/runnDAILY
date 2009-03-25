b4dce3fab4f62e610400b5fa9ca99f5565fbe436
byron wall <byron@byroni.us>
2009-03-25 21:04:08 +0000

    Quick hack so that route distance is correct decimal places.

--- system/class/route.php
+++ system/class/route.php
@@ -19,6 +19,7 @@ class Route extends Object{
 		parent::__construct($arr, $arr_pre);
 		
 		$this->creation = strtotime($this->creation);
+		$this->distance = round($this->distance, 2);
 	}
 
 	/**
@@ -254,7 +255,7 @@ class Route extends Object{
 	private static function _removeImage($rid){
 		$img_src = ($rid % 100) . "/" . $rid . ".png";
 		$path = PUBLIC_ROOT . "/img/route/" . $img_src;
-		return unlink($path);		
+		return unlink($path);
 	}
 	private function _storeImage(){
 		$img_src = ($this->id % 100) . "/" . $this->id . ".png";
