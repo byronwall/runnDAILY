6d35382632cb2e881aa31a1ab6e85ca6ee4a8dcc
chandler <chandler@bce454a7-8c10-b24f-9796-9f7183d4e6d1>
2009-06-20 01:42:28 +0000

    This branch will be used to try out a few new site designs.

---
+++ branches/XXX - New Design/source/system/class/smartyext.php
@@ -0,0 +1,18 @@
+<?php
+class SmartyExt extends Smarty{
+	
+	function __construct(){
+		$this->template_dir = SYSTEM_ROOT."/tpl";
+		$this->compile_dir = CLASS_ROOT."/smarty/templates_c";
+		$this->cache_dir = CLASS_ROOT."/smarty/cache";
+		$this->config_dir = CLASS_ROOT."/smarty/configs";
+		$this->left_delimiter = "{{";
+		$this->right_delimiter = "}}";
+	}
+	
+	public function display_master($tpl){
+		$this->assign("page_content", $this->fetch($tpl));
+		$this->display("master.tpl");
+	}
+}
+?>
